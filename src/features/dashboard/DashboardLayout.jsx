4fa7a4f9369558dd9424a2385692d82f2e710d82
Aman Kushwaha <amankushwaha7284@gmail.com>
2025-12-17 13:36:51 +0530

    line chart & pie chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -25,8 +27,8 @@ function DashboardLayout() {
     <StyledDashboardLayout>
      <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} cabinCount={cabins.length}/>
       <div>Today's Activity</div>
-      <div>Chart stay durations</div>
-      <div>Chart sale</div>
+      <DurationChart confirmedStays={confirmedStays} />
+      <SalesChart bookings={bookings} numDays={numDays}/>
 
     </StyledDashboardLayout>
   )
